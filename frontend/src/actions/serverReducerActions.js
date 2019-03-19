2d786f5e262f555297a98f259cd53bb1a8a1e557
mariusu <marius.sundnes@gmail.com>
2019-03-19 12:51:46 +0100

    SideMenu uses data from server

--- frontend/src/actions/serverReducerActions.js
+++ frontend/src/actions/serverReducerActions.js
@@ -4,9 +4,8 @@ import * as types from "../actionTypes/serverReducerTypes"
 export const getKpiList = () => {
   return dispatch => {
     dispatch(getKpiListStarted())
-
     axios
-      .get("http://localhost:4000/kpi-list")
+      .get("http://localhost:4000/kpi-list/list")
       .then(res => {
         dispatch(getKpiListSuccess(res.data))
       })
@@ -15,7 +14,24 @@ export const getKpiList = () => {
       })
   }
 }
-
-const getKpiListSuccess = data => ({type: types.GET_KPI_LIST_SUCCESS, payload: {...data}})
 const getKpiListStarted = () => ({type: types.GET_KPI_LIST_STARTED})
+const getKpiListSuccess = data => ({type: types.GET_KPI_LIST_SUCCESS, payload: {...data}})
 const getKpiListFailure = error => ({type: types.GET_KPI_LIST_FAILURE, payload: {error}})
+
+
+export const getKpiCategories = () => {
+  return dispatch => {
+    dispatch(getKpiCategoriesStarted())
+    axios
+      .get("http://localhost:4000/kpi-list/categories")
+      .then(res => {
+        dispatch(getKpiCategoriesSuccess(res.data))
+      })
+      .catch(err => {
+        dispatch(getKpiCategoriesFailure(err.message))
+      })
+  }
+}
+const getKpiCategoriesStarted = () => ({type: types.GET_KPI_CATEGORIES_STARTED})
+const getKpiCategoriesSuccess = data => ({type: types.GET_KPI_CATEGORIES_SUCCESS, payload: {...data}})
+const getKpiCategoriesFailure = error => ({type: types.GET_KPI_CATEGORIES_FAILURE, payload: {error}})
