40bb60cff97d6e745db94e1a4bdd6205dfc0bac0
ncdanielsen <ncdanielsen@live.co.uk>
2019-03-25 17:31:37 +0100

    neighbourhood_schema now takes building objects

--- mock-api/schemas/neighbourhood_schema.js
+++ mock-api/schemas/neighbourhood_schema.js
@@ -4,10 +4,5 @@ const Schema = mongoose.Schema;
 
 module.exports.neighbourhood_schema = new Schema({
     name: "String",
-    location: [{
-        type: "String"
-    }],
-    buildings: [{ 
-        type: "String"
-    }]
+    buildings: [{ type: Schema.Types.ObjectId, ref: 'Buildings'}]
 });
