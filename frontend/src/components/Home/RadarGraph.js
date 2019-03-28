43d44159e4356247af5c598dd4ae857e6ef49ea3
mariusu <marius.sundnes@gmail.com>
2019-03-28 15:08:54 +0100

    Standardize indentation

--- frontend/src/components/Home/RadarGraph.js
+++ frontend/src/components/Home/RadarGraph.js
@@ -7,24 +7,24 @@ import { Radar, RadarChart, PolarGrid, PolarAngleAxis, Tooltip, Legend } from 'r
 
 export default class RadarGraph extends Component {
 
-    constructor(props) {
-        super(props);
-      }
-
-    render() {
-        return (
-            <div>
-                <RadarChart cx={this.props.chartSize*0.5*1.2} cy={this.props.chartSize*0.5} outerRadius={this.props.chartSize*0.3} 
-                            width={this.props.chartSize*1.2} height={this.props.chartSize} 
-                            data={this.props.currentKpisSelected.map(selectKpi => this.props.cKpis.find(cKpi => cKpi.name === selectKpi))}>
-                    <PolarGrid />
-                    <PolarAngleAxis dataKey="name" />
-                    <Radar name="Calculated" dataKey="score" stroke="#e034de" fill="#f145ef" fillOpacity={0.6} />
-                    <Tooltip />
-                    <Legend />
-                </RadarChart>
-            </div>
-        );
-    }
+  render() {
+    return (
+        <div>
+            <RadarChart
+              cx={this.props.chartSize*0.5*1.2}
+              cy={this.props.chartSize*0.5}
+              outerRadius={this.props.chartSize*0.3}
+              width={this.props.chartSize*1.2}
+              height={this.props.chartSize}
+              data={this.props.currentKpisSelected.map(selectKpi => this.props.cKpis.find(cKpi => cKpi.name === selectKpi))}
+            >
+                <PolarGrid />
+                <PolarAngleAxis dataKey="name" />
+                <Radar name="Calculated" dataKey="score" stroke="#e034de" fill="#f145ef" fillOpacity={0.6} />
+                <Tooltip />
+                <Legend />
+            </RadarChart>
+        </div>
+    );
+  }
 }
-
