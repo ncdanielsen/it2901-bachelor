2aa0b8f954007cb0ccd15ac8a99b9ab978c3d821
mariusu <marius.sundnes@gmail.com>
2019-03-28 13:31:16 +0100

    Split MyData into several reusable components

--- frontend/src/components/Home/MyData.js
+++ frontend/src/components/Home/MyData.js
@@ -5,7 +5,8 @@ import styles from './MyData.module.css'
 
 import { replace } from 'connected-react-router'
 
-import moment from 'moment'
+import UploadNewKpiSet from './UploadNewKpiSet'
+import KpiSetListItem from './KpiSetListItem'
 
 function mapStateToProps(state) {
   return {
@@ -41,48 +42,20 @@ class MyData extends Component {
     return (
       <div className={styles.myDataContainer}>
         <div onClick={this.close} className={styles.closeButtonMyData}>X</div>
-        <div className={styles.uploadButton}>Upload New</div>
-        <div className={styles.orTextContainer}>
-          <div className={styles.separationLine} />
-          <div className={styles.orText}>or choose from<br/>existing sets of calculated KPIs</div>
-        </div>
+        <UploadNewKpiSet uploadNew={() => console.log("uploadNew")} />
         <div className={styles.kpiSets}>
           {this.props.kpiSets.map((kpiSet, index) => (
-            <div key={index} className={styles.kpiSet}>
-              <div className={styles.kpiSection}>
-                <div className={styles.kpiNameLabel}>Name</div>
-                <div className={styles.kpiName}>{kpiSet.name}</div>
-                <div className={styles.kpiDates}>
-                  <div>
-                    Created: {moment(kpiSet.created).format('DD.MM.YY')}
-                  </div>
-                  <div>
-                    Last updated: {moment(kpiSet.lastUpdated).format('DD.MM.YY')}
-                  </div>
-                </div>
-              </div>
-              <div className={styles.kpiSection}>
-                <div className={styles.kpiNameLabel}>Building</div>
-                <div className={styles.kpiName}>{kpiSet.building.name}</div>
-                <div onClick={() => this.viewBuildingDetails(kpiSet.name)} className={styles.viewDetailsButton}>
-                  View Details
-                </div>
-              </div>
-              <div className={styles.kpiButtonSection}>
-                <div className={styles.kpiOptionTop}>
-                  <div onClick={() => this.editKpiSet(kpiSet.name)} className={styles.editButton}>
-                    Edit
-                  </div>
-                </div>
-                <div onClick={() => this.selectKpiSet(kpiSet.name)} className={styles.kpiOptionBottom}>
-                  <div className={styles.selectButton}>
-                    Select
-                  </div>
-                </div>
-              </div>
-            </div>
+            <KpiSetListItem
+              key={index}
+              isCalculatedKpi={true}
+              kpiSet={kpiSet}
+              editKpiSet={() => this.editKpiSet(kpiSet.name)}
+              selectKpiSet={() => this.selectKpiSet(kpiSet.name)}
+              viewBuildingDetails={() => this.viewBuildingDetails(kpiSet.name)}
+            />
           ))}
         </div>
+        <div className={styles.paddingBottom} />
       </div>
     )
   }
