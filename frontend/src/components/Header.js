597cab50f322fd0cc0dadd3f98c466a79471e646
Andreas Bergmo Johnsen <andrbjoh@stud.ntnu.no>
2019-04-01 14:56:32 +0200

    added button that will be pressed to slide in the menu

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,43 +1,64 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
-import { push } from 'connected-react-router'
+import { push } from "connected-react-router";
 
-import zenLogo from '../images/zen.png'
-import fmeLogo from '../images/fme.png'
+import zenLogo from "../images/zen.png";
+import fmeLogo from "../images/fme.png";
+import menuLogo from "../images/menu.png"; //taken from https://material.io/tools/icons/?icon=menu&style=round
 
 function mapStateToProps(state) {
-  return {}
+  return {};
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    push: (newUrl) => dispatch(push(newUrl))
-  }
+    push: newUrl => dispatch(push(newUrl))
+  };
 }
 
 class Header extends Component {
-
   render() {
     return (
       <div className={styles.Header}>
-        <div onClick={()=>this.props.push("/")} className={styles.HeaderLogo+' '+styles.link}>
+        <div
+          onClick={() => this.props.push("/")}
+          className={styles.HeaderLogo + " " + styles.link}
+        >
           <img src={zenLogo} className={styles.logo} alt="zen logo" />
           <img src={fmeLogo} className={styles.logo} alt="fme logo" />
         </div>
         <div className={styles.headerRightSide}>
-          <div onClick={()=>this.props.push("/about")} className={styles.link}>
+          <div
+            onClick={() => this.props.push("/about")}
+            className={styles.link}
+          >
             About
           </div>
-          <div onClick={()=>this.props.push("/profile")} className={styles.link}>
+          <div
+            onClick={() => this.props.push("/profile")}
+            className={styles.link}
+          >
             Profile
           </div>
         </div>
+
+        <div className={styles.HeaderLeftSide}>
+          <div
+            onClick={() => this.props.push("/")} //TODO: collapse the menu and go fullscreen. Also add vica versa
+            className={styles.HeaderLogo + " " + styles.link}
+          >
+            <img src={menuLogo} className={styles.logo} alt="menu logo" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
