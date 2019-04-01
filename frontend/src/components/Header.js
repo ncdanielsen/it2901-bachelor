4323a60d4fe283c680056ad9e10b73342625c240
mariusu <marius.sundnes@gmail.com>
2019-04-01 16:12:39 +0200

    Hamburger icon is in place

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,30 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from "react"
+import { connect } from "react-redux"
 
-import styles from "./Header.module.css";
+import styles from "./Header.module.css"
 
-import { push } from "connected-react-router";
+import { push } from "connected-react-router"
 
-import zenLogo from "../images/zen.png";
-import fmeLogo from "../images/fme.png";
-import menuLogo from "../images/menu.png"; //taken from https://material.io/tools/icons/?icon=menu&style=round
+import zenLogo from "../images/zen.png"
+import fmeLogo from "../images/fme.png"
+import menuLogo from "../images/menu.png" //taken from https://material.io/tools/icons/?icon=menu&style=round
 
 function mapStateToProps(state) {
-  return {};
+  const pathname = state.router.location.pathname
+  return {
+    showHamburger: (pathname === '/home/' || pathname === "/home/myData" || pathname === "/home/refData")
+  }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    push: newUrl => dispatch(push(newUrl))
-  };
+    push: newUrl => dispatch(push(newUrl)),
+    toggleShowSideMenu: () => console.log("toggleShowSideMenu here")
+  }
 }
 
 class Header extends Component {
+
   render() {
     return (
       <div className={styles.Header}>
@@ -30,6 +35,12 @@ class Header extends Component {
           <img src={zenLogo} className={styles.logo} alt="zen logo" />
           <img src={fmeLogo} className={styles.logo} alt="fme logo" />
         </div>
+        {this.props.showHamburger && <div
+          onClick={() => this.props.toggleShowSideMenu()}
+          className={styles.HeaderLogo + " " + styles.link}
+        >
+          <img src={menuLogo} className={styles.humburger} alt="menu logo" />
+        </div>}
         <div className={styles.headerRightSide}>
           <div
             onClick={() => this.props.push("/about")}
@@ -45,20 +56,12 @@ class Header extends Component {
           </div>
         </div>
 
-        <div className={styles.HeaderLeftSide}>
-          <div
-            onClick={() => this.props.push("/")} //TODO: collapse the menu and go fullscreen. Also add vica versa
-            className={styles.HeaderLogo + " " + styles.link}
-          >
-            <img src={menuLogo} className={styles.logo} alt="menu logo" />
-          </div>
-        </div>
       </div>
-    );
+    )
   }
 }
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Header);
+)(Header)
