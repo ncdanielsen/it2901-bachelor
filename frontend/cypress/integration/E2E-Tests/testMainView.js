90e4ea62a50bff0e0fce5cf3872990f773565cee
ncdanielsen <ncdanielsen@live.co.uk>
2019-05-02 13:09:35 +0200

    Fixed tests

--- frontend/cypress/integration/E2E-Tests/testMainView.js
+++ frontend/cypress/integration/E2E-Tests/testMainView.js
@@ -8,7 +8,7 @@ context("Test MainView", () =>{
         })
 
         it("Try to view single kpi in graph with reference values", function(){
-            cy.contains("Select a KPI to view from the side menu")
+            cy.contains("Welcome")
             cy.contains("My Data Source").click()
             cy.contains('Select').first().click()
             cy.contains('Reference Data').click()
@@ -21,7 +21,7 @@ context("Test MainView", () =>{
         })
         
         it("Try to view multiple kpi's in graph with reference values", function(){
-            cy.contains("Select a KPI to view from the side menu")
+            cy.contains("Welcome")
             cy.contains("My Data Source").click()
             cy.contains('Select').first().click()
             cy.contains('Reference Data').click()
@@ -36,7 +36,7 @@ context("Test MainView", () =>{
         })
 
         it("Try to view multiple kpi's in both Radargraph and Linegraph", function(){
-            cy.contains("Select a KPI to view from the side menu")
+            cy.contains("Welcome")
             cy.contains("My Data Source").click()
             cy.contains('Select').first().click()
             cy.contains('Reference Data').click()
@@ -47,14 +47,14 @@ context("Test MainView", () =>{
             cy.get('#kpiBoxes > :nth-child(2)').click()
             cy.get('#kpiBoxes > :nth-child(3)').click()
             cy.get('#graphContainerId').should('be.visible')
+            cy.get('#lineGraphId').should('be.visible')
+            cy.contains('Chart Type').trigger("mouseover")
+            cy.contains('Line')
+            cy.contains('Radar').click()
+            cy.get('#graphContainerId').should('be.visible')
             cy.get('#radarGraphId').should('be.visible')
             cy.contains('Calculated KPI').should('be.visible')
             cy.contains('Reference KPI').should('be.visible')
-            cy.contains('Chart Type').trigger("mouseover")
-            cy.contains('Radar')
-            cy.contains('Line').click()
-            cy.get('#graphContainerId').should('be.visible')
-            cy.get('#lineGraphId').should('be.visible')
             
         })
     })
