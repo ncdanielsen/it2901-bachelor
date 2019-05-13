8e7744e6b47b4c99d1bf6204630d102856ad74e2
Nils <ncdanielsen@live.co.uk>
2019-05-14 01:23:03 +0200

    Added som additional documentation

--- mock-api/schemas/category_schema.js
+++ mock-api/schemas/category_schema.js
@@ -4,7 +4,16 @@ const Schema = mongoose.Schema;
 
 module.exports.kpi_cat_schema = new Schema({
     name: String,
-    cat_id: Number,
+    cat_id: Number, //
     children: [{ type: Schema.Types.ObjectId, ref: 'KPI Metadata'}],
-    restricted: {type: "boolean", default: false, required: true}
 });
+
+
+/**
+ * Schema of a KPI Category
+ * 
+ * name:            String          Description: The name of the kpi category. Example: "Energy", "Power", "Emissions", etc
+ * cat_id:          Number          Description: Identifier for the category.
+ * children:        []              Description: Array that contains the id's of kpi which belong to this category. These kpi are defined in "kpi_meta_schema.js".
+ * 
+ */
--- mock-api/schemas/user_schema.js
+++ mock-api/schemas/user_schema.js
@@ -1,10 +1,23 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    superuser: { type: "boolean", default: false}, 
-    admin: {type: "boolean", default: false}
+    _id: mongoose.Schema.Types.ObjectId, 
+    email: {type: String, required: true, unique: true}, 
+    password: {type: String, required: true}, 
+    superuser: { type: "boolean", default: false},                                   
+    admin: {type: "boolean", default: false}      
 })
 module.exports = mongoose.model("user_schema", userSchema, "users_TEST");
+
+/**
+ * _id:         mongoose.Schema.Types.ObjectId          Description: Automatic and unique id created by mongoose.
+ * email:       String                                  Description: The users email in string format. Is unique, and required for creating a user.
+ * password:    String                                  Description: The users password. Stored in plaintext. Required in order to create user.
+ *                                                                   No real passwords should currently be used when creating user.
+ * superuser:   boolean                                 Description: Boolean meant to indicate if a user can view restricted data or not , however, 
+ *                                                                   this functionality is currently not implemented. Is still used in "../users/profile" function. 
+ * admin:       boolean                                 Description: Boolean meant to indicate if a user should have admin powers, however, 
+ *                                                                   this functionality is currently not implemented. Is still used in "../users/profile" function.  
+ * 
+ */
+    
